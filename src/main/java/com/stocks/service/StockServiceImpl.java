d27fe18a34ab7ea5c230647d7d5f0f092daed3dc
echo <1355874389@qq.com>
2026-07-24 02:54:09 +0000

    Break addStock for CI demo

--- src/main/java/com/stocks/service/StockServiceImpl.java
+++ src/main/java/com/stocks/service/StockServiceImpl.java
@@ -26,7 +26,7 @@ public class StockServiceImpl implements StockService {
     @Override
     public Stock addStock(Stock stock) {
         stockRepository.findBySymbol(stock.symbol()).ifPresent(s -> {
-            throw new IllegalArgumentException("Duplicate symbol: " + stock.symbol());
+            throw new IllegalArgumentException("Always broken: " + stock.symbol());
         });
         return stockRepository.save(stock);
     }
